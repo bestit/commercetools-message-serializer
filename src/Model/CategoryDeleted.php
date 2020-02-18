dd4bef8e5bd48d0ead1ca994fcbaf673483da93c
Michel Chowanski <michel@chowanski.de>
2020-02-18 14:12:40 +0100

    DEN-101: Add models for each delivery action for customer, product and category

---
+++ src/Model/CategoryDeleted.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Messenger\Model;
+
+use Commercetools\Core\Model\Subscription\ResourceDeletedDelivery;
+
+/**
+ * Model for deleted category
+ *
+ * @author Michel Chowanski <michel.chowanski@bestit-online.de>
+ * @package BestIt\Messenger\Model
+ */
+class CategoryDeleted extends ResourceDeletedDelivery
+{
+}
---
+++ src/Model/ProductDeleted.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Messenger\Model;
+
+use Commercetools\Core\Model\Subscription\ResourceDeletedDelivery;
+
+/**
+ * Model for deleted product
+ *
+ * @author Michel Chowanski <michel.chowanski@bestit-online.de>
+ * @package BestIt\Messenger\Model
+ */
+class ProductDeleted extends ResourceDeletedDelivery
+{
+}
