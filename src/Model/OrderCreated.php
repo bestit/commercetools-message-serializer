a8ad0e3f5b81657f76ce5bc10e1f3787562626cf
André Varelmann <andre.varelmann@bestit-online.de>
2020-08-06 15:09:49 +0200

    DEN-1198 Added order models.

---
+++ src/Model/OrderCreated.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Messenger\Model;
+
+use Commercetools\Core\Model\Subscription\ResourceCreatedDelivery;
+
+/**
+ * Model for created order
+ *
+ * @package BestIt\Messenger\Model
+ */
+class OrderCreated extends ResourceCreatedDelivery
+{
+}
---
+++ src/Model/OrderDeleted.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\Messenger\Model;
+
+use Commercetools\Core\Model\Order\Order;
+use Commercetools\Core\Model\Subscription\ResourceDeletedDelivery;
+
+/**
+ * Model for deleted order
+ *
+ * @package BestIt\Messenger\Model
+ */
+class OrderDeleted extends ResourceDeletedDelivery
+{
+}
